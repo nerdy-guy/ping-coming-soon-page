3a4e511de466d1344fdb4bcf0ab0df53cbff4256
nerdy-guy <115644166+nerdy-guy@users.noreply.github.com>
2022-12-21 08:04:07 +0200

    change border color on invalid email

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const form = document.querySelector("#form");
 const email = document.querySelector("#email");
 const validation = document.querySelector("#validation");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  validateInput();
-});
-
 const validateInput = () => {
   const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
@@ -14,5 +9,11 @@ const validateInput = () => {
     location.reload();
   } else {
     validation.textContent = "Please provide a valid email address";
+    email.style.borderColor = "var(--clr-light-red)";
   }
 };
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInput();
+});
