09196690184a5670a5963c479cf364bfa1d8ca43
nerdy-guy <115644166+nerdy-guy@users.noreply.github.com>
2022-12-21 19:03:27 +0200

    add message for valid inputs

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const validateInput = () => {
   const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
   if (pattern.test(email.value)) {
-    location.reload();
+    validation.textContent = "You will be notified";
+    validation.style.color = "var(--clr-green)";
+    validation.style.marginRight = "16em";
+    email.value = "";
+    email.style.borderColor = "var(--clr-green)";
   } else if (email.value === "") {
     validation.textContent =
       "Whoops! It looks like you forgot to add your email";
+    validation.style.color = "var(--clr-light-red)";
     email.style.borderColor = "var(--clr-light-red)";
   } else {
     validation.textContent = "Please provide a valid email address";
+    validation.style.color = "var(--clr-light-red)";
+    validation.style.marginRight = "8em";
     email.style.borderColor = "var(--clr-light-red)";
   }
 };
